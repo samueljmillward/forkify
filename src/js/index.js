0e29cbffe4ea75e1f342e7b08afbbd3a7f039e8c
samueljmillward <samuel.j.pearce@hotmail.com>
2020-09-01 19:14:49 +0100

    Babel/Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 // Global app controller
+import x from './test';
+const z = 23;
+
+console.log(`I imported ${x} from another module! Variable x is ${x}`)
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('Imported Module');
+export default 234567890;
