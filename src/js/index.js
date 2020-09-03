a20c0353febc0826d37e2cbb2f27363b9c4d3b85
samueljmillward <samuel.j.pearce@hotmail.com>
2020-09-03 18:35:06 +0100

    Recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -59,8 +59,10 @@ const controlRecipe = async () => {
         state.recipe = new Recipe(id);
 
         try {
-            //Get recipe data
+            //Get recipe data and parse ingredients
             await state.recipe.getRecipe();
+            console.log(state.recipe.ingredients);
+            state.recipe.parseIngredients();
 
             //Calc servings and time
             state.recipe.calcTime();
