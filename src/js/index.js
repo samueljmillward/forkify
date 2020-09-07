3cae86972f52952c8b14a6f93d2dffd75233d649
samueljmillward <samuel.j.pearce@hotmail.com>
2020-09-07 13:58:44 +0100

    Recipe UI increases/decreases ingredients reqs dependent on servings

--- src/js/index.js
+++ src/js/index.js
@@ -84,4 +84,18 @@ const controlRecipe = async () => {
     }
 };
 
-['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe)); 
+['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
+
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if
+        (e.target.matches('.btn-increase, .btn-increase *')) {
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+});
